c2a8fc6ed5fc968d699eef3b6f3ab9d61e95b67d
Michael Anderson <Micmail566@gmail.com>
2017-02-06 17:09:07 -0500

    Loaded a theme

--- FanMVC/App_Start/BundleConfig.cs
+++ FanMVC/App_Start/BundleConfig.cs
@@ -25,6 +25,7 @@ namespace FanMVC
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/Theme.css",
                       "~/Content/site.css"));
         }
     }
